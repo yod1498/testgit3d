64ef5b49030f27d8a1f540e42a1361a412511f36
yod <yod1498@yahoo.com>
2017-04-08 11:11:00 +0700

    First commit

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += 
+			new Vector3 (Input.GetAxis("Horizontal"),0,0);
+		
+	}
+}
